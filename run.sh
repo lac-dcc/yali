2abf9810e6fa241fdae2f2d7175ec8f2a7d74aef
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-13 16:29:47 -0300

    Read and write volume

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
-MODEL=$1            # cnn | rf
-TRAINDATASET=$2     # OJClone | BCF| FLA | SUB | OLLVM | MCMC | GA | DRLSG | RS (or the same names with the suffix "O3")
-OPTLEVEL=$3         # O0 | O3
-NUMCLASSES=$4       # 104 | 10
-TESTDATASET=$3      # OJClone | BCF| FLA | SUB | OLLVM | | MCMC | GA | DRLSG | RS (or the same names with the suffix "O3")
+set -e
+
+MODEL=$1            # [cnn, rf]
+TRAINDATASET=$2     # [OJClone, BCF| FLA, SUB, OLLVM, MCMC, GA, DRLSG, RS] (or the same names with the suffix "O3")
+OPTLEVEL=$3         # [O0, O3]
+NUMCLASSES=$4       # [104, 10]
+TESTDATASET=$3      # [OJClone, BCF| FLA, SUB, OLLVM,, MCMC, GA, DRLSG, RS] (or the same names with the suffix "O3")
+
+# # Download the OJClone dataset
+# wget --quiet [link] -O Volume/Source/OJClone.tar.xz
+# tar -xf Volume/Source/OjClone.tar.xz
+# rm Volume/Source/OJClone.tar.xz
 
 # Python Env for the statistics
 python3 -m venv .venv
@@ -15,6 +22,6 @@ mkdir -p ./Volume/Source
 
 # Run classification model
 docker run --rm \
-    -v /home/thais/Documents/Master/DL/Obfuscation/Volume:/yali/Dataset \
+    -v /home/thais/Documents/Master/DL/Obfuscation/Volume:/yali/Dataset:rw \
     yali:ubuntu-20.04 /bin/bash \
     -c "./yali/Classification/Classify.sh ${MODEL} ${TRAINDATASET} ${OPTLEVEL} ${NUMCLASSES} ${TESTDATASET}"
