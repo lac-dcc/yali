dc3b2b9bb16df98062aec21790e7720632bb671f
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-13 10:06:57 -0300

    Necessary folders

--- run.sh
+++ run.sh
@@ -4,6 +4,13 @@ OPTLEVEL=$3         # O0 | O3
 NUMCLASSES=$4       # 104 | 10
 TESTDATASET=$3      # OJClone | BCF| FLA | SUB | OLLVM | | MCMC | GA | DRLSG | RS (or the same names with the suffix "O3")
 
+# Creating main directories
+mkdir -p ./Volume/Csv
+mkdir -p ./Volume/Histograms
+mkdir -p ./Volume/Results
+mkdir -p ./Volume/Source
+
+# Run classification model
 docker run --rm \
     -v /home/thais/Documents/Master/DL/Obfuscation/Volume:/yali/Dataset \
     yacos:ubuntu-20.04 /bin/bash \
