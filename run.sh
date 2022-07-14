85599fd53fcc3a607fc3a30b7a220c3881c9c180
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-14 19:52:22 -0300

    Script to setup the repo

--- run.sh
+++ run.sh
@@ -6,20 +6,6 @@ OPTLEVEL=$3         # [O0, O3]
 NUMCLASSES=$4       # [104, 10]
 TESTDATASET=$3      # [OJClone, BCF| FLA, SUB, OLLVM,, MCMC, GA, DRLSG, RS] (or the same names with the suffix "O3")
 
-# # Download the OJClone dataset
-# wget --quiet [link] -O Volume/Source/OJClone.tar.xz
-# tar -xf Volume/Source/OjClone.tar.xz
-# rm Volume/Source/OJClone.tar.xz
-
-# Python Env for the statistics
-python3 -m venv .venv
-
-# Creating main directories
-mkdir -p ./Volume/Csv
-mkdir -p ./Volume/Histograms
-mkdir -p ./Volume/Results
-mkdir -p ./Volume/Source
-
 # Run classification model
 docker run --rm \
     -v /home/thais/Documents/Master/DL/Obfuscation/Volume:/yali/Dataset:rw \
