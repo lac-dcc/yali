7a15e393c0f8e09cb59a627399b532fcf7199e2b
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-14 19:52:22 -0300

    Script to setup the repo

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# # Download the OJClone dataset
+# wget --quiet [link] -O Volume/Source/OJClone.tar.xz
+# tar -xf Volume/Source/OjClone.tar.xz
+# rm Volume/Source/OJClone.tar.xz
+
+# Python Env for the statistics
+python3 -m venv .venv
+
+# Creating main directories
+mkdir -p ./Volume/Csv
+mkdir -p ./Volume/Histograms
+mkdir -p ./Volume/Results
+mkdir -p ./Volume/Source
