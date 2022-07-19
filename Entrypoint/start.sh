dd26553ad777627eaa4c2f29a88088005f1c293a
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-19 12:22:17 -0300

    Permissions to the container

--- Entrypoint/start.sh
+++ Entrypoint/start.sh
@@ -8,5 +8,9 @@ OPTLEVEL=$4
 NUMCLASSES=$5
 TESTDATASET=$6
 
-sudo chown -R ml4code ${VOLUME}
+sudo chown -R ml4code ${VOLUME}/Csv
+sudo chown -R ml4code ${VOLUME}/Histograms
+sudo chown ml4code ${VOLUME}/Irs
+sudo chown -R ml4code ${VOLUME}/Results
+sudo chown -R ml4code ${VOLUME}/Source
 ./yali/Classification/Classify.sh ${MODEL} ${TRAINDATASET} ${OPTLEVEL} ${NUMCLASSES} ${TESTDATASET}
