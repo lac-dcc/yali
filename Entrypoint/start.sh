ecf51b5636949f98e9e5089898a6848f36125f20
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-19 19:51:37 -0300

    Fix classifier

--- Entrypoint/start.sh
+++ Entrypoint/start.sh
@@ -4,9 +4,10 @@ set -e
 VOLUME=$1
 MODEL=$2
 TRAINDATASET=$3
-OPTLEVEL=$4
+OPTLEVELTRAIN=$4
 NUMCLASSES=$5
 TESTDATASET=$6
+OPTLEVELTEST=$4
 
 sudo chmod 755 ${VOLUME}
 sudo chown -R ml4code:sudo ${VOLUME}/Csv
@@ -14,4 +15,4 @@ sudo chown -R ml4code:sudo ${VOLUME}/Histograms
 sudo chown ml4code:sudo ${VOLUME}/Irs
 sudo chown -R ml4code:sudo ${VOLUME}/Results
 sudo chown -R ml4code:sudo ${VOLUME}/Source
-./yali/Classification/Classify.sh ${MODEL} ${TRAINDATASET} ${OPTLEVEL} ${NUMCLASSES} ${TESTDATASET}
+./yali/Classification/Classify.sh ${MODEL} ${TRAINDATASET} ${OPTLEVELTRAIN} ${NUMCLASSES} ${TESTDATASET} ${OPTLEVELTEST}
