0398eb5ac0bb73482844659a46b34fdf8a8694b5
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-19 12:36:24 -0300

    Add logs

--- setup.sh
+++ setup.sh
@@ -4,13 +4,19 @@
 # rm Volume/Source/OJClone.tar.xz
 
 # Python Env for the statistics
+echo "Creating python environment..."
 python3 -m venv .venv
+echo "Environment created."
 
 # Creating main directories
+echo "Creating folders of the volume..."
 mkdir -p ./Volume/Csv
 mkdir -p ./Volume/Histograms
 mkdir -p ./Volume/Results
 mkdir -p ./Volume/Source
+echo "Folders created."
 
 # Creating the docker image
+echo "Creating docker image..."
 docker compose build
+echo "Image created."
