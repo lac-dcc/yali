ec68aff3f3e3de2da61a30092f00d3817d926cd0
Thais Damasio <thais.damasio@outlook.com.br>
2022-07-21 10:08:46 -0300

    Organize the classify script

--- Classification/Classify.sh
+++ Classification/Classify.sh
@@ -7,6 +7,7 @@ NUMCLASSES=$4
 TESTDATASET=$5
 OPTLEVELTEST=$6
 
+# Set Parameters 
 checkParameters() {
     if [ -z "${MODEL}" ]; then
         echo "Error: No model specified!"
@@ -30,24 +31,38 @@ checkParameters() {
     fi
 }
 
+# Create the histograms
 histograms() {
     local setName=$1
     local optType=$2
     local irFolder=~/yali/Dataset/Irs/${setName}${optType}/
     local csvFile=~/yali/Dataset/Csv/features_${setName}${optType}.csv
-    local outputDir=~/yali/Histograms/${setName}${optType}/
+    local csvFinished=~/yali/Dataset/Csv/Finished_${setName}${optType}
+    local outputDir=~/yali/Dataset/Histograms/${setName}${optType}
 
-    echo "===> Creating histograms ${setName}..."
-    make -C ${irFolder}
-    echo "===> Histograms finished ${setName} <==="
+    # Histogram CSV
+    touch ${csvFinished}
+    if [ -z "$(cat ${csvFinished})" ]; then
+        echo "===> Creating histograms ${setName}..."
+        make -C ${irFolder}
+        echo "===> Histograms finished ${setName} <==="
+        echo "1" > ${csvFinished}
+    fi
 
-    echo "===> Converting CSV to Numpy ${setName}..."
-    python3 ~/yali/Extraction/ConvertCSVToNP.py \
-        --histogramCSV ${csvFile} \
-        --outputDir ${outputDir}
-    echo "===> Conversion finished ${setName} <==="
+    # Histogram Numpy Format
+    mkdir -p ${outputDir}
+    touch -p ${outputDir}/Finished
+    if [ -z "$(cat ${outputDir}/Finished)" ]; then
+        echo "===> Converting CSV to Numpy ${setName}..."
+        python3 ~/yali/Extraction/ConvertCSVToNP.py \
+            --histogramCSV ${csvFile} \
+            --outputDir ${outputDir}/
+        echo "1" > ${outputDir}/Finished
+        echo "===> Conversion finished ${setName} <==="
+    fi
 }
 
+# Create the programs
 compiling() {
     local setName=$1
     local optType=$2
@@ -55,37 +70,41 @@ compiling() {
     local scriptFolder=~/yali/Compilation/
 
     mkdir -p ${irFolder}
+    touch ${irFolder}/Finished
 
-    if [ -z "$(ls -A ${irFolder})" ]; then
+    if [ -z "$(cat ${irFolder}/Finished)" ]; then
         echo "===> Compiling ${setName}..."
 
         if [ "${setName}" = "BCF" ] || [ "${setName}" = "FLA" ] || [ "${setName}" = "SUB" ] || [ "${setName}" = "OLLVM" ]; then
             source ${scriptFolder}/CompileOLLVM.sh ${optType} ${setName}
             echo "===> Compilation finished <==="
-            histograms ${setName} ${optType}
         else
             source ${scriptFolder}/Compile.sh ${setName} ${optType}
             echo "===> Compilation finished <==="
-            histograms ${setName} ${optType}
         fi
     fi
+
+    histograms ${setName} ${optType}
 }
 
+# Classification process
 classification() {
     local trainName=$1
     local optTypeTrain=$2
     local testName=$3
     local optTypeTest=$4
-    local resultsPath=~/yali/Dataset/Results/${trainName}${optTypeTrain}
+    local resultsPath=~/yali/Dataset/Results/${trainName}${optTypeTrain}/
     local trainDir=~/yali/Dataset/Histograms/${trainName}${optTypeTrain}/
     local testDir=~/yali/Dataset//Histograms/${testDir}${optTypeTest}/
 
     if [ -z ${testName} ]; then
+        echo "===> Classification: train and testing phase (${setName}${opTypeTrain}) ..."
         python3 ~/yali/Classification/VectorTTClassify.py \
             --train_dataset_directory ${trainDir} \
             --results_directory ${resultsPath} \
             --model ${MODEL}
     else
+        echo "===> Classification: training phase (${setName}${opTypeTrain}) --- testing phase (${testDir}${optTypeTest}) ..."
         python3 ~/yali/Classification/VectorTTClassify.py \
             --train_dataset_directory ${trainDir} \
             --train_p 100 \
@@ -93,6 +112,7 @@ classification() {
             --results_directory ${resultsPath}_${testName}${optTypeTest} \
             --model ${MODEL}
     fi
+    echo "===> Classification finished <==="
 }
 
 
@@ -101,10 +121,10 @@ checkParameters
 if [ -z ${TESTDATASET} ]; then
     compiling ${TRAINDATASET} ${OPTLEVELTRAIN}
 
-    classification ${TRAINDATASET}
+    classification ${TRAINDATASET} ${OPTLEVELTRAIN}
 else
     compiling ${TRAINDATASET} ${OPTLEVELTRAIN}
     compiling ${TESTDATASET} ${OPTLEVELTEST}
  
-    classification ${TRAINDATASET} ${TESTDATASET}
+    classification ${TRAINDATASET} ${OPTLEVELTRAIN} ${TESTDATASET} ${OPTLEVELTEST}
 fi
