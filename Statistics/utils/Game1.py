036ab5db8c17f9aba92d872c8351a50665a336ad
Thais Damasio <thais.damasio@outlook.com.br>
2022-08-14 21:12:28 -0300

    Fix bugs

--- Statistics/utils/Game1.py
+++ Statistics/utils/Game1.py
@@ -49,56 +49,56 @@ def getSeparateCharts(titulo, fig, data0, models, labelY, xLabels, metricType):
         dict: Data about the Game 1
     """
     game1 = {}
-    game, data = Game.getAveragesByModels(STRATEGIES[0], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[1], models, metricType)
     game1["O0+OLLVM"] = game
     ax1 = Game.makeSingleChart(
         letter="a", df=data, fig=fig, coord=241, legendData="O0+OLLVM", 
         titulo=titulo, labelY=labelY, game0=data0, hideX=True, xLabels=xLabels
     )
 
-    game, data = Game.getAveragesByModels(STRATEGIES[1], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[2], models, metricType)
     game1["O0+OLLVMFLA"] = game
     Game.makeSingleChart(
         letter="b", df=data, fig=fig, coord=242, legendData="O0+OLLVMFLA", xLabels=xLabels,
         titulo=titulo, labelY=labelY, shareAxY=ax1, game0=data0, hideY=True, hideX=True
     )
 
-    game, data = Game.getAveragesByModels(STRATEGIES[2], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[3], models, metricType)
     game1["O0+O3"] = game
     Game.makeSingleChart(
         letter="c", df=data, fig=fig, coord=243, legendData="O0+O3", xLabels=xLabels,
         titulo=titulo, labelY=labelY, shareAxY=ax1, game0=data0, hideY=True, hideX=True
     )
 
-    game, data = Game.getAveragesByModels(STRATEGIES[3], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[4], models, metricType)
     game1["O0+MCMC"] = game
     Game.makeSingleChart(
         letter="d", df=data, fig=fig, coord=244, legendData="O0+MCMC", xLabels=xLabels,
         titulo=titulo, labelY=labelY, shareAxY=ax1, game0=data0, hideY=True, hideX=True
     )
 
-    game, data = Game.getAveragesByModels(STRATEGIES[4], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[5], models, metricType)
     game1["O0+OLLVMSUB"] = game
     ax5 = Game.makeSingleChart(
         letter="e", df=data, fig=fig, coord=245, legendData="O0+OLLVMSUB", xLabels=xLabels,
         titulo=titulo, labelY=labelY, game0=data0
     )
 
-    game, data = Game.getAveragesByModels(STRATEGIES[5], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[6], models, metricType)
     game1["O0+OLLVMBCF"] = game
     Game.makeSingleChart(
         letter="f", df=data, fig=fig, coord=246, legendData="O0+OLLVMBCF", xLabels=xLabels,
         titulo=titulo, labelY=labelY, shareAxY=ax5, game0=data0, hideY=True
     )
     
-    game, data = Game.getAveragesByModels(STRATEGIES[6], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[7], models, metricType)
     game1["O0+DRLSG"] = game
     Game.makeSingleChart(
         letter="g", df=data, fig=fig, coord=247, legendData="O0+DRLSG", xLabels=xLabels,
         titulo=titulo, labelY=labelY, shareAxY=ax5, game0=data0, hideY=True
     )
     
-    game, data = Game.getAveragesByModels(STRATEGIES[7], models, metricType)
+    game, data = Game.getAveragesByModels(STRATEGIES[8], models, metricType)
     game1["O0+RS"] = game
     Game.makeSingleChart(
         letter="h", df=data, fig=fig, coord=248, legendData="O0+RS", xLabels=xLabels,
