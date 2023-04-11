e2bc27ec13e94dd194b3b82af443d185d5827802
Thais Damasio <thais.damasio@outlook.com.br>
2023-04-11 08:52:54 -0300

    Script to run the jotai experiment

---
+++ Statistics/Experiments/JotaiOpcodes/JotaiOpcodes.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+ROOTPATH=$(dirname $(realpath "$0"))/../../..
+
+YALI=http://cuda.dcc.ufmg.br/Yali
+YC='\033[0;33m'
+NC='\033[0m'
+
+getDataset() {
+    echo -e "${YC}===> Downloading the datasets...${NC}"
+    wget ${YALI}/jotai.tar.xz -O $ROOTPATH/Volume/Source/jotai.tar.xz
+
+    echo -e "${YC}===> Unpacking...${NC}"
+    tar -xf $ROOTPATH/Volume/Source/jotai.tar.xz -C $ROOTPATH/Volume/Source/
+
+    rm $ROOTPATH/Volume/Source/jotai.tar.xz
+}
+
+playGame() {
+    SCRIPTFOLDER=/home/ml4code/yali/Classification
+    local modelName=$1
+    local obfStrategy=$2
+    local optType=$3
+
+    DOCKER_BUILDKIT=1 docker run -v ${ROOTPATH}/Volume:/home/ml4code/yali/Dataset \
+                yali_yali ${SCRIPTFOLDER}/BinaryClassification.sh 10 $modelName "jotai" $obfStrategy $optType
+}
+
+jotaiClassification() {
+    MODELS=( rf ) #cnn rf mlp svm knn lr )
+    OBFSTRATEGY=( FLA BCF SUB OLLVM )
+    OPTLEVEL=( O1 O2 O3 )
+    
+    for m in "${!MODELS[@]}"; do
+        for o in "${!OBFSTRATEGY[@]}"; do
+            playGame ${MODELS[$m]} ${OBFSTRATEGY[$o]} "O0"
+        done
+    done
+
+    for m in "${!MODELS[@]}"; do
+        for o in "${!OPTLEVEL[@]}"; do
+            playGame ${MODELS[$m]} "None" ${OPTLEVEL[$o]}
+        done
+    done
+}
+
+# getDataset
+jotaiClassification
