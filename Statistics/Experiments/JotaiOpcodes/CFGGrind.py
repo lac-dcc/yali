5d57da494fdc4c18ce03a2a8e33f8f9c0d4d177b
Thais Damasio <thais.damasio@outlook.com.br>
2023-05-29 00:03:06 -0300

    Generates histogram (numpy format)

--- Statistics/Experiments/JotaiOpcodes/CFGGrind.py
+++ Statistics/Experiments/JotaiOpcodes/CFGGrind.py
@@ -20,15 +20,23 @@ parser.add_argument(
     "--dataset", type=str, required=True,
     help="Directory name with the IRs (Jotai) to get the histogram")
 parser.add_argument("--clang", type=str, required=True, help="Path to clang")
+parser.add_argument(
+    "--baselineCSVStatic", type=str, required=False,
+    help="CSV path for static histograms compiled with -O0")
+parser.add_argument(
+    "--baselineCSVDynamic", type=str, required=False,
+    help="CSV path for dynamic histograms compiled with -O0")
 args = parser.parse_args()
 
+
 def _GetCFGGrindStatistics(
-        file_name: str, obj_path: str, csv_static: List[Dict],
+        file_name: str, class_code: str, obj_path: str, csv_static: List[Dict],
         csv_dynamic: List[Dict]):
     """Gets statistics (json format) for `obj_path`.
 
     Args:
         file_name: Name of the file
+        class_code: Class of the program
         obj_path: Path to the object
         csv_static: List to add histogram with static opcodes
         csv_dynamic: List to add histogram with dynamic opcodes
@@ -37,7 +45,7 @@ def _GetCFGGrindStatistics(
     function_name = re.sub("_Final..*", "", temp)
 
     command = (f"cfggrind_info -s functions -f \"{obj_path}::{function_name}\" "
-    + f"-i {obj_path}.map -m json {obj_path}.cfg")
+               + f"-i {obj_path}.map -m json {obj_path}.cfg")
 
     out_raw = subprocess.run(
         command, capture_output=True, shell=True, check=True)
@@ -47,7 +55,9 @@ def _GetCFGGrindStatistics(
         out_json = out_json[0]
 
         out_json["static"]["instructions"]["opcodes"]["id"] = file_name
+        out_json["static"]["instructions"]["opcodes"]["class"] = class_code
         out_json["dynamic"]["instructions"]["opcodes"]["id"] = file_name
+        out_json["dynamic"]["instructions"]["opcodes"]["class"] = class_code
 
         csv_static.append(out_json["static"]["instructions"]["opcodes"])
         csv_dynamic.append(out_json["dynamic"]["instructions"]["opcodes"])
@@ -65,7 +75,7 @@ def _Compile(
         file_path: Path to the IR file
         out_path: Path to the folder to save the output
         clang_path: Path to clang
-    
+
     Returns:
         Path to the object file if the compilation was successful, `None`
         otherwise
@@ -118,7 +128,7 @@ def _GenerateHistogramsUsingCFGGrind(
         dataset_name: Name of the dataset with the IR files
         clang_path: Path to Clang
         path_to_dots: Path to save the dot files
-    
+
     Returns:
         Tuple with:
             - List with static histograms
@@ -150,7 +160,7 @@ def _GenerateHistogramsUsingCFGGrind(
                     _CreateMap(out_file)
                     _CreateDot(out_file, path_to_dots)
                     _GetCFGGrindStatistics(
-                        file, out_file, csv_dynamic, csv_static)
+                        file, subdir_name, out_file, csv_dynamic, csv_static)
 
     return csv_static, csv_dynamic
 
@@ -177,14 +187,41 @@ def _InstallCFGGrind(valgrind_version: str, path_to_build: str):
     os.system("sudo make install")
 
 
-def _SaveCsvs(
-        csv_dynamic: List[Dict], csv_static: List[Dict], dataset_name: str):
-    """Saves the CSVs on hard disk.
+def _CallHistogramScript(
+        baseline_csv: str, csv_path: str, out_folder_name: str):
+    """Calls script python to extract histograms (numpy format)
+
+    Args:
+        baseline_csv: CSV path for histograms compiled with -O0
+        csv_path: Path to CSV with histograms
+        out_folder_name: Output directory name
+    """
+    out_dir = f"{PATH_TO_YALI}/Volume/Histograms/{out_folder_name}"
+    os.makedirs(out_dir, exist_ok=True)
+    ext_features = "loop_depth_1 loop_depth_2 loop_depth_3 loop_depth_n num_bbs"
+
+    command = (f"python3 {PATH_TO_YALI}/Extraction/Utils/ConvertCSVsToNP.py " +
+        f"--histogramCSV1 {baseline_csv} " +
+        f"--histogramCSV2 {csv_path} " +
+        f"--outputDir {out_dir} " +
+        f"--remove {ext_features}")
+    os_result = os.system(command)
+
+    if os_result != 0:
+        print("Err extracting histogram (numpy format)!")
+
+
+def _SaveHistograms(
+        csv_dynamic: List[Dict], csv_static: List[Dict],
+        dataset_name: str, baseline_sta_csv: str, baseline_dyn_csv: str):
+    """Saves the CSVs and Histograms (numpy format) on hard disk.
 
     Args:
         csv_dynamic: Data of the CSV with dynamic histograms
         csv_static: Data of the CSV with static histograms
         dataset_name: Name of the dataset
+        baseline_sta_csv: CSV path for static histograms compiled with -o0
+        baseline_dyn_csv: CSV path for dynamic histograms compiled with -o0
     """
     df_static = pd.DataFrame(csv_static).set_index("id")
     df_static = df_static.fillna(0).astype(int)
@@ -192,25 +229,37 @@ def _SaveCsvs(
     df_dynamic = pd.DataFrame(csv_dynamic).set_index("id")
     df_dynamic = df_dynamic.fillna(0).astype(int)
 
-    df_static.to_csv(
-        f"{PATH_TO_YALI}/Volume/Csv/features_CFGGRIND-D{dataset_name}.csv")
-    df_dynamic.to_csv(
-        f"{PATH_TO_YALI}/Volume/Csv/features_CFGGRIND-S{dataset_name}.csv")
+    dyn_name = f"CFGGRIND-D{dataset_name}"
+    sta_name = f"CFGGRIND-S{dataset_name}"
+    dyn_csv = f"{PATH_TO_YALI}/Volume/Csv/features_{dyn_name}.csv"
+    sta_csv = f"{PATH_TO_YALI}/Volume/Csv/features_{sta_name}.csv"
+
+    df_static.to_csv(dyn_csv)
+    df_dynamic.to_csv(sta_csv)
+
+    if baseline_sta_csv is not None:
+        _CallHistogramScript(baseline_sta_csv, sta_csv, sta_name)
+    if baseline_dyn_csv is not None:
+        _CallHistogramScript(baseline_dyn_csv, dyn_csv, dyn_name)
 
 
-def GetDynamicHistograms(dataset_name: str, clang_path: str):
+def GetDynamicHistograms(
+        dataset_name: str, clang_path: str,
+        baseline_sta_csv: str, baseline_dyn_csv: str):
     """Generates histograms from `dataset` through a dynamic analysis.
 
     Args:
         dataset_path: Path to the dataset
         clang_path: Path to clang
+        baseline_sta_csv: CSV path for static histograms compiled with -o0
+        baseline_dyn_csv: CSV path for dynamic histograms compiled with -o0
     """
     if which(clang_path) is None:
         raise ValueError("Clang cannot be found!")
 
     valgrind_version = "valgrind-3.20.0"
     cfggrind_path = f"{PATH_TO_SCRIPT}/build"
-    path_to_dots = f"{cfggrind_path}/build/dots"
+    path_to_dots = f"{cfggrind_path}/dots"
     os.makedirs(path_to_dots, exist_ok=True)
 
     if which("cfggrind_asmmap") is None:
@@ -219,11 +268,15 @@ def GetDynamicHistograms(dataset_name: str, clang_path: str):
     csv_static, csv_dynamic = _GenerateHistogramsUsingCFGGrind(
         dataset_name, clang_path, path_to_dots)
 
-    _SaveCsvs(csv_dynamic, csv_static, dataset_name)
+    _SaveHistograms(
+        csv_dynamic, csv_static, dataset_name, baseline_sta_csv,
+        baseline_dyn_csv)
 
 
 if __name__ == "__main__":
     dataset = args.dataset
     clang = args.clang
+    baselineCSVStatic = args.baselineCSVStatic
+    baselineCSVDynamic = args.baselineCSVDynamic
 
-    GetDynamicHistograms(dataset, clang)
+    GetDynamicHistograms(dataset, clang, baselineCSVStatic, baselineCSVDynamic)
